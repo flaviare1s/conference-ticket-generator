d39caca6676b3e45ef21db24965c458ba21f21d5
flaviare1s <flaviarster@gmail.com>
2024-12-23 09:33:44 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import { Footer } from './components/Footer'
+
+export function App() {
+
+  return (
+    <>
+      <main>
+        {/* <!-- Form starts --> */}
+
+        Your Journey to Coding Conf 2025 Starts Here!
+
+        Secure your spot at next year's biggest coding conference.
+
+        Upload Avatar
+        Drag and drop or click to upload
+        Upload your photo (JPG or PNG, max size: 500KB).
+
+        Full Name
+
+        Email Address
+        example@email.com
+
+        GitHub Username
+        @yourusername
+
+        Generate My Ticket
+
+        {/* <!-- Form ends --> */}
+
+        {/* <!-- Generated tickets starts --> */}
+
+        Congrats, ...! Your ticket is ready.
+
+        We've emailed your ticket to ... and will send updates in the run up to the event.
+
+        Coding Conf
+        Jan 31, 2025 / Austin, TX
+
+        {/* <!-- Generated tickets ends --> */}
+      </main>
+      <Footer />
+    </>
+  )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <div className="text-center text-[11px]">
+      Challenge by <a className="font-bold" href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+      Coded by <a className="font-bold" href="https://github.com/flaviare1s">Flávia Reis</a>.
+    </div>
+  )
+}
